f415c30c4b7b18fb1a58ad6f7ab609b96fa51003
alainpail <alainpaillier59@gmail.com>
2025-02-27 15:50:22 +0100

    modif

--- compillateur_PAILLIER_CHADID/src/PtGen.java
+++ compillateur_PAILLIER_CHADID/src/PtGen.java
@@ -117,7 +117,7 @@ public class PtGen {
     // -------------------------
     
  // MERCI de renseigner ici un nom pour le trinome, constitue EXCLUSIVEMENT DE LETTRES
-    public static String trinome="XxxYyyZzz"; 	//TODO 
+    public static String trinome="PAILLIER Alan et CHADID Hafsa"; 	//TODO 
     
     private static int tCour; // type de l'expression compilee
     private static int vCour; // sert uniquement lors de la compilation d'une valeur (entiere ou boolenne)
